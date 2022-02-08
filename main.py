ac501104fc4928e40f3b3361edc10c1dea1fe22e
S Daut <92053137+NameUser5@users.noreply.github.com>
2022-02-08 14:56:16 +0000

    LUNCH! :D

--- main.py
+++ main.py
@@ -12,10 +12,7 @@ if start == "YES":
 if start != "YES":
   calc_run = False
   print("Alas, now is not the time for trickery. Goodbye and have a jolly good day!")
-# if not start == "YES" or "NO": 
-#   calc_run = False
-#   print("Alas, now is not the time for games. Goodbye.")
-    
+
   
 while calc_run == True:
 
@@ -34,15 +31,7 @@ while calc_run == True:
     total = num1 / num2
     return total
     
-    
-  # try:
-  #   total = num1 / num2
-  #   return total
-  # except ZeroDivisionError:
-  #   print("Impossible.")
-
-    
-  # num1 = float(input("Enter your first number: ")) 
+  
   try:
     num1 = float(input("Enter your first number: ")) 
     valid_num1 = True
@@ -60,7 +49,7 @@ while calc_run == True:
       valid_opp = False
 
   while valid_opp == False:
-    operation = str(input("Try again. Enter an appropriate symbol."))
+    operation = str(input("Try again. Enter an appropriate symbol. "))
     
     if operation in operations:
       valid_opp = True
@@ -72,7 +61,7 @@ while calc_run == True:
     print("Nice try.")
     valid_num2 = False
   while valid_num2 == False:
-    # num2 = float(input("Enter your second number: "))
+    
     try:
       num2 = float(input("Enter your second number: ")) 
       valid_num2 = True
@@ -80,47 +69,7 @@ while calc_run == True:
       print("Nice try.")
       valid_num2 = False
       continue
-    # if not ValueError:
-    #   valid_num2 = True
     
-  # num2 = float(input("Enter your second number: "))
-  
-
-  # if num2 >= 0:
-  #   valid_num2 = True
-  # elif num2 <0:
-  #   valid_num2 = True
-  # else:
-  #   valid_num2 = False
-  
-  # while valid_num2 == False:
-  #   num2 = float(input("Enter your second number: "))
-    
-  #   if num2 >=0 or num2 < 0:
-  #     valid_num2 = True
-    
-  # try:
-  #   num2 = float(input("Enter your second number: ")) 
-  #   valid_num2 = True
-  # except ValueError:
-  #   print("I admire your efforts.")
-  #   valid_num2 = False
-  #   continue
-  # num2 = float(input("Enter your second number: ")) 
-
-  # operations = ["+","-","*","/"]
-
-  # for o in operations:
-  #   if opp in operations:
-  #     valid_opp = True
-  #   else:
-  #     valid_opp = False
-
-      # while valid_opp == False:
-      #   operation = str(input("Try again. Enter an appropriate symbol.")
-
-  # for n in num2:
-
 
   for o in operations:
     if operation == "+":
@@ -134,9 +83,5 @@ while calc_run == True:
         result = divide(num1,num2)
       else: 
         result = "Undefined. You cannot divide by zero"
-      # try:
-      #   result = divide(num1,num2)
-      # except ZeroDivisionError:
-      #   print("Impossible.")
-      #   continue
-  print(f"\nYour total is: {result}.")
+     
+  print(f"\nYour total is: {result}.\n")
